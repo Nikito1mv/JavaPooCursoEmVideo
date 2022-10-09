c41d705f3114a295d3db9a252653d687bb0aedaa
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-10-09 18:56:36 -0300

    q7

--- Javapoo/src/aulaA7/Lutador.java
+++ Javapoo/src/aulaA7/Lutador.java
@@ -1,13 +1,147 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aulaA7;
-
-/**
- *
- * @author Micael
- */
 public class Lutador {
     
+    // Atribultos
+    
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String Categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    
+    // Métodos Especiais
+
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.setPeso(peso);
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    private void setCategoria() {
+        String x = null;
+        if (peso < 52.2){
+            x = "Inválido";
+        } else if (peso <= 70.3) {
+            x = "Peso Leve";
+        } else if (peso <= 83.9) {
+            x = "Peso Médio";
+        } else if (peso <= 120.2) {
+            x = "Peso Pesado";
+        } else {
+            x = "Inválido";
+        } this.Categoria = x;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+    
+    // Métodos
+    
+    public void apresentar() {
+        System.out.println("--------------------------------------");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Nacionalidade: " + this.getNacionalidade());
+        System.out.println("Idade: " + this.getIdade());
+        System.out.println("Altura: " + this.getAltura());
+        System.out.println("Peso: " + this.getPeso());
+        System.out.println("Categoria: " + this.getCategoria());
+        System.out.println("Vitorias: " + this.getVitorias());
+        System.out.println("Derrotas: " + this.getDerrotas());
+        System.out.println("Empates: " + this.getEmpates());
+    }
+    
+    public void status() {
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Categoria: " + this.getCategoria());
+        System.out.println("Vitorias: " + this.getVitorias());
+        System.out.println("Derrotas: " + this.getDerrotas());
+        System.out.println("Empates: " + this.getEmpates());
+    }
+    
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+    
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+    
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
 }
